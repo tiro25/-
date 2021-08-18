1780a6f3fa54526f89d373817105d9ee058d18d5
tiro25 <cju0425@naver.com>
2021-08-18 23:39:50 +0900

    0818 add boj twopointer

---
+++ Algorithm/Solution/backjoon/two_pointer/G4_2473_세용액.java
@@ -0,0 +1,57 @@
+package boj._gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class G4_2473_세용액 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
+    int fst = 0, snd = 0, trd = 0;
+    if(arr[0] >= 0) {
+      fst = arr[0];
+      snd = arr[1];
+      trd = arr[2];
+    } else if(arr[n-1] <= 0) {
+      fst = arr[n-3];
+      snd = arr[n-2];
+      trd = arr[n-1];
+    } else {
+      long min = (long)3e9;
+      for(int i = 0; i < n-2; ++i) {
+        for(int j = i+1; j < n-1; ++j) {
+          int two = arr[i]+arr[j];
+          int lo = j+1, hi = n;
+          while(lo < hi) {
+            int mid = (lo+hi)/2;
+            long sum = 0L+two+arr[mid];
+
+            if(Math.abs(sum) < min) {
+              min = Math.abs(sum);
+              fst = arr[i];
+              snd = arr[j];
+              trd = arr[mid];
+            }
+
+            if(sum > 0) {
+              hi = mid;
+            } else if(sum < 0) {
+              lo = mid+1;
+            } else {
+              i = n;
+              j = n;
+              break;
+            }
+          }
+        }
+      }
+    }
+    System.out.println(fst + " " + snd + " " + trd);
+  }
+}
---
+++ Algorithm/Solution/backjoon/two_pointer/G5_2467_용액.java
@@ -0,0 +1,44 @@
+package boj._gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class G5_2467_용액 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+    int fir = 0, sec = 0;
+    if(arr[0] >= 0) {
+      fir = arr[0];
+      sec = arr[1];
+    } else if(arr[n-1] <= 0) {
+      fir = arr[n-2];
+      sec = arr[n-1];
+    } else {
+      int left = 0, right = n-1;
+      int min = (int)2e9;
+      while(left < right) {
+        int sum = arr[left]+arr[right];
+        if(Math.abs(sum) < min) {
+          min = Math.abs(sum);
+          fir = arr[left];
+          sec = arr[right];
+        }
+        if(sum > 0) {
+          right--;
+        } else if(sum < 0) {
+          left++;
+        } else {
+          break;
+        }
+      }
+    }
+    System.out.println(fir + " " + sec);
+  }
+}
